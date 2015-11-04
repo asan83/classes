ef3c8d28ead4a116e689e8424c44c046110ff420
FedorH <fedorhajdu@gmail.com>
2015-11-04 11:11:50 +0100

    Add allocate_seat method

--- airtravel.py
+++ airtravel.py
@@ -2,7 +2,8 @@
 
 
 class Flight:
-    def __init__(self, number):
+    """A flight with a particular passenger aircraft."""
+    def __init__(self, number, aircraft):
         if not number[:2].isalpha():
             raise ValueError("No airline code in '{}'".format(number))
         
@@ -13,16 +14,55 @@ class Flight:
             raise ValueError("Invalid route number '{}'".format(number))
         
         self._number = number
+        self._aircraft = aircraft
+
+        rows, seats = self._aircraft.seating_plan()
+        # "[None] + " appends a single null element at the start of the list because list is zero based
+        # _ in rows discards the row number
+        self._seating = [None] + [{letter:None for letter in seats} for _ in rows]
         
     def number(self):
         return self._number
     
     def airline(self):
         return self._number[:2]
-    
+
+    def aircraft_model(self):
+        return self._aircraft.model()
+
+    def allocate_seat(self, seat, passenger):
+        """Allocate a seat to a passenger.
+
+        Args:
+            seat: A seat designator such as '12C' or '21F'.
+            passenger: passenger name.
+
+        Raises:
+            ValueError: If the seat is unavailable
+        """
+        rows, seat_letters = self._aircraft.seating_plan()
+
+        letter = seat[-1] # get a seat letter by using negative indexing
+        if letter not in seat_letters:
+            raise ValueError("Invalid seat letter {}".format(letter))
+
+        row_text = seat[:-1] # take all but the last character
+        try:
+            row = int(row_text)
+        except ValueError:
+            raise ValueError("Invalid seat row {}".format(row_text))
+
+        if row not in rows:
+            raise ValueError("Invalid row number".format(row))
+
+        if self._seating[row][letter] is not None:
+            raise ValueError("Seat {} already occupied".format(seat))
+
+        self._seating[row][letter] = passenger
+
 
 class Aircraft:
-    def __init__(self, registration, model, num_rows, num_seats_per_row): 
+    def __init__(self, registration, model, num_rows, num_seats_per_row):
         self._registration = registration
         self._model = model
         self._num_rows = num_rows

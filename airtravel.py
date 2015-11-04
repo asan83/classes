36fb6d05a3e6087e621519377a9a9d74b2e43583
FedorH <fedorhajdu@gmail.com>
2015-11-04 14:27:40 +0100

    Add functionlity for printing boarding cards
    This example shows a basic polymorphism and why
    Python doesn't need interfaces.

--- airtravel.py
+++ airtravel.py
@@ -84,6 +84,19 @@ class Flight:
         self._seating[to_row][to_letter] = self._seating[from_row][from_letter]
         self._seating[from_row][from_letter] = None
 
+    def make_boarding_cards(self, card_printer):
+        for passenger, seat in sorted(self._passenger_seats()):
+            card_printer(passenger, seat, self.number(), self.aircraft_model())
+
+    def _passenger_seats(self):
+        """An iterable series of passenger seating allocations."""
+        row_numbers, seat_letters = self._aircraft.seating_plan()
+        for row in row_numbers:
+            for letter in seat_letters:
+                passenger = self._seating[row][letter]
+                if passenger is not None:
+                    yield (passenger, "{}{}".format(row, letter))
+
     def num_available_seats(self):
         return sum(sum(1 for s in row.values() if s is None)
                    for row in self._seating
@@ -116,3 +129,17 @@ def make_flight():
     f.allocate_seat('1C', 'Some Random Person')
     f.allocate_seat('1D', 'A Passenger Steve')
     return f
+
+
+def console_card_printer(passenger, seat, flight_number, aircraft):
+    output = "| Name: {0}"  \
+             "  Flight: {1}" \
+             "  Seat: {2}" \
+             "  Aircraft {3}" \
+             " |".format(passenger, flight_number, seat, aircraft)
+    banner = "+" + "-" * (len(output) - 2) + '+'
+    border = "|" + " " * (len(output) - 2) + '|'
+    lines = [banner, border, output, border, banner]
+    card = '\n'.join(lines)
+    print(card)
+    print()

d2766eed8155f9f273503d42181189ea82e1a16c
FedorH <fedorhajdu@gmail.com>
2015-11-04 14:12:38 +0100

    add relocate passenger method and sample data for testing

--- airtravel.py
+++ airtravel.py
@@ -3,27 +3,28 @@
 
 class Flight:
     """A flight with a particular passenger aircraft."""
+
     def __init__(self, number, aircraft):
         if not number[:2].isalpha():
             raise ValueError("No airline code in '{}'".format(number))
-        
+
         if not number[:2].isupper():
             raise ValueError("Invalid airline code '{}'".format(number))
-        
+
         if not (number[2:].isdigit() and int(number[2:]) <= 9999):
             raise ValueError("Invalid route number '{}'".format(number))
-        
+
         self._number = number
         self._aircraft = aircraft
 
         rows, seats = self._aircraft.seating_plan()
         # "[None] + " appends a single null element at the start of the list because list is zero based
         # _ in rows discards the row number
-        self._seating = [None] + [{letter:None for letter in seats} for _ in rows]
-        
+        self._seating = [None] + [{letter: None for letter in seats} for _ in rows]
+
     def number(self):
         return self._number
-    
+
     def airline(self):
         return self._number[:2]
 
@@ -33,11 +34,11 @@ class Flight:
     def _parse_seat(self, seat):
         rows, seat_letters = self._aircraft.seating_plan()
 
-        letter = seat[-1] # get a seat letter by using negative indexing
+        letter = seat[-1]  # get a seat letter by using negative indexing
         if letter not in seat_letters:
             raise ValueError("Invalid seat letter {}".format(letter))
 
-        row_text = seat[:-1] # take all but the last character
+        row_text = seat[:-1]  # take all but the last character
         try:
             row = int(row_text)
         except ValueError:
@@ -65,6 +66,29 @@ class Flight:
 
         self._seating[row][letter] = passenger
 
+    def relocate_passenger(self, from_seat, to_seat):
+        """Relocate a passenger to a different seat.
+
+        Args:
+            from_seat: The existing seat designator for the passenger to be moved
+            to_seat: New seat designator
+        """
+        from_row, from_letter = self._parse_seat(from_seat)
+        if self._seating[from_row][from_letter] is None:
+            raise ValueError("No passenger to relocate in seat {}".format(from_seat))
+
+        to_row, to_letter = self._parse_seat(to_seat)
+        if self._seating[to_row][to_letter] is not None:
+            raise ValueError("Seat {} is already occupied.".format(to_seat))
+
+        self._seating[to_row][to_letter] = self._seating[from_row][from_letter]
+        self._seating[from_row][from_letter] = None
+
+    def num_available_seats(self):
+        return sum(sum(1 for s in row.values() if s is None)
+                   for row in self._seating
+                   if row is not None)
+
 
 class Aircraft:
     def __init__(self, registration, model, num_rows, num_seats_per_row):
@@ -82,3 +106,13 @@ class Aircraft:
     def seating_plan(self):
         return (range(1, self._num_rows + 1),
                 "ABCDEFGHJK"[:self._num_seats_per_row])
+
+
+def make_flight():
+    f = Flight("BA758", Aircraft("G-EPPE", "Airbus A319", 22, 6))
+    f.allocate_seat('12A', "Test passenger")
+    f.allocate_seat('15F', "Different Passenger")
+    f.allocate_seat('15E', "Another Person")
+    f.allocate_seat('1C', 'Some Random Person')
+    f.allocate_seat('1D', 'A Passenger Steve')
+    return f
